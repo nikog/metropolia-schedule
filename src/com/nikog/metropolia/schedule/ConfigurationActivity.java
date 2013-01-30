c3941e2d1f1a13e1f7a0e22520a49b3bc2e3b011
Niko Gröhn <niko.grohn@gmail.com>
2013-01-30 18:58:05 +0200

    Configuration now shows current values.

--- src/com/nikog/metropolia/schedule/ConfigurationActivity.java
+++ src/com/nikog/metropolia/schedule/ConfigurationActivity.java
@@ -4,13 +4,17 @@ import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
-public class ConfigurationActivity extends PreferenceActivity {
+public class ConfigurationActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	public static final String PREFS_NAME = "com.nikog.metropolia.schedule.widget.configuration";
+	
+	private EditTextPreference mGroupPreference;
 
 	int widgetId;
 
@@ -30,6 +34,27 @@ public class ConfigurationActivity extends PreferenceActivity {
 		
 		// Default result
 		setResult(RESULT_CANCELED);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		String group = prefs.getString("group", null);
+		
+		if(group != null) {
+			mGroupPreference = (EditTextPreference) findPreference("group");
+			
+			mGroupPreference.setSummary(group);	
+		}
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
 	}
 
 	// For now, back button will accept the settings and create the widget
@@ -68,5 +93,12 @@ public class ConfigurationActivity extends PreferenceActivity {
 		Intent serviceIntent = new Intent(ctx, UpdateService.class);
 		serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
 		ctx.startService(serviceIntent);	
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		if(key.equals("group")) {
+			mGroupPreference.setSummary(sharedPreferences.getString(key, null));
+		}
 	}
 }
